8dd5e981515423c7c3d33716f93b751afc6fb617
oyakata <oyakata@users.noreply.github.com>
2016-10-31 14:26:28 +0900

    First commit/hello world.

---
+++ src/hello.go
@@ -0,0 +1,12 @@
+package main
+
+import fmt "fmt"
+
+func main() {
+    message := "Hello, world. [今川義元]"
+
+    for i:= 0; i < 10; i++ {
+        // fmt.Printf(message)
+        fmt.Println(message)
+    }
+}
