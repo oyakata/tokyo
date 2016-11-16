ea6d6578863b9a1b58d5f2e749dddd695f7e0e8b
oyakata <oyakata@users.noreply.github.com>
2016-11-16 12:22:07 +0900

    無名関数の作り方を勉強中
    可変長引数
    http://reiki4040.hatenablog.com/entry/2014/09/15/211922
    
    無名関数
    http://qiita.com/high5/items/4e2580241039c950e1c4

---
+++ src/firstcls.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main(){
+	var foo = func(x int) int {
+		return x + 5
+	}
+
+	var deco = func(fn func(int) int) func(int) int {
+		return func(x int) int {
+			return fn(x)
+		}
+	}
+
+	var fn = deco(foo)
+	fmt.Println(fn(100))
+}
