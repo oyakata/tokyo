24745b65213aa5938ddbbb6b939ac088234f4660
oyakata <oyakata@users.noreply.github.com>
2016-12-05 15:12:06 +0900

    ユニットテスト途中経過

--- err/main.go
+++ err/main.go
@@ -1,17 +1,53 @@
 package err
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func PositiveInt(x int) error {
 	if x < 0 {
-		return fmt.Errorf("PositiveInt() argument must be a positive number, not negative")
+		return fmt.Errorf("PositiveInt() argument must be a positive number, not '%v'", x)
 	}
 	return nil
 }
 
 func Even(x int) error {
 	if x%2 == 0 {
-		return fmt.Errorf("Even() argument must not be a odd number.")
+		return fmt.Errorf("Even() argument must be a odd number, not '%v'.", x)
 	}
 	return nil
+}
+
+// 合計値を返す。但し、引数に負の数が含まれたら即座に処理を打ち切ってnilを返す。
+func Sum(xs ...int) (result int, e error) {
+	var err error
+	for _, x := range xs {
+		err = PositiveInt(x)
+		if err != nil {
+			if e == nil {
+				e = errors.New("Sum")
+			}
+			e = fmt.Errorf("%v: %v", e, err)
+			return 0, e
+		}
+		result += x
+	}
+	return
+}
+
+// 合計を求める。但し、引数に負の数が含まれても計算を続行する。
+func SumEasily(xs ...int) (result int, e error) {
+	var err error
+	for _, x := range xs {
+		err = PositiveInt(x)
+		if err != nil {
+			if e == nil {
+				e = errors.New("SumEasily")
+			}
+			e = fmt.Errorf("%v: %v", e, err)
+		}
+		result += x
+	}
+	return
 }
--- err/main_test.go
+++ err/main_test.go
@@ -25,3 +25,28 @@ func TestPositiveInt(t *testing.T) {
 		t.Errorf("%vにもかかわらずエラーになりました", param)
 	}
 }
+
+func TestSum(t *testing.T) {
+	var xs []int
+	var result int
+	var err error
+
+	xs = []int{1, 2, 3, 4, 5}
+	result, err = Sum(xs...)
+	if err != nil {
+		t.Error(err)
+	}
+	if result != 15 {
+		t.Error("Sum(%v) != 15, but %v", xs, result)
+	}
+
+	xs = []int{10, 11, -5, 8, -2, 16, 0, -9}
+	result, err = Sum(xs...)
+	if err == nil {
+		t.Errorf("Sum(%v) should be failed, but succeeded.", xs)
+	}
+	if result != 0 {
+		t.Error("Sum(%v) != 0, but %v", xs, result)
+	}
+
+}
