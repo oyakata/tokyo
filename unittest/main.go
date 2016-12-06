3cfd988916e52d0a9d5802ea7951f528052c21a2
oyakata <oyakata@users.noreply.github.com>
2016-12-06 18:28:37 +0900

    ファイルを書き出すテストを追加。

--- unittest/main.go
+++ unittest/main.go
@@ -39,9 +39,21 @@ func CSVToMaps(in io.Reader) (xs []Entry, err error) {
 }
 
 // ローカルのファイルを出力するプログラム (S3とかに出力する場合はこの方法ではダメ)
-func WriteCSV(names []string, []Entry, filepath string) {
-	w := os.OpenFile(filepath, os.O_WRONLY | os.O_CREATE | os.O_APPEND, 0644)
-	w.Write()
+func WriteCSV(names []string, xs []Entry, filepath string) {
+	out, _ := os.OpenFile(filepath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	defer out.Close()
+	w := csv.NewWriter(out)
+
+	defer w.Flush()
+	w.Write(names)
+
+	values := make([]string, len(names))
+	for _, x := range xs {
+		for i := 0; i < len(names); i++ {
+			values[i] = x[names[i]]
+		}
+		w.Write(values)
+	}
 }
 
 // ログを出力するプログラム
--- unittest/main_test.go
+++ unittest/main_test.go
@@ -1,6 +1,8 @@
 package unittest
 
 import (
+	"io/ioutil"
+	"os"
 	"reflect"
 	"strings"
 	"testing"
@@ -31,4 +33,35 @@ func TestCSVToMaps(t *testing.T) {
 }
 
 // ファイルを出力するプログラムのテスト: ioutil.TempFileで対処
-func Test
+func TestWriteCSV(t *testing.T) {
+	tmp, _ := ioutil.TempFile("", "_")
+	defer func() {
+		os.Remove(tmp.Name())
+	}()
+
+	data := []Entry{
+		{"id": "1", "name": "foo", "age": "18"},
+		{"id": "2", "name": "bar", "age": ""},
+		{"id": "3", "name": "", "age": "5"},
+	}
+	names := []string{"id", "name", "age"}
+	WriteCSV(names, data, tmp.Name())
+
+	in, _ := os.Open(tmp.Name())
+	defer in.Close()
+	bs, err := ioutil.ReadAll(in)
+	if err != nil {
+		t.Errorf("処理に失敗しました: %v", err)
+	}
+
+	result := string(bs)	
+
+	expected := ("" +
+		"id,name,age\n" +
+		"1,foo,18\n" +
+		"2,bar,\n" +
+		"3,,5\n")
+	if result != expected {
+		t.Errorf("結果が期待通りではありませんでした: %v != %v", result, expected)
+	}
+}
