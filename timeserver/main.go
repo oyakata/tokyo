c776ca26842661668ee81bfecf6b30002e7e7fa0
oyakata <oyakata@users.noreply.github.com>
2016-12-12 10:27:24 +0900

    時刻表示サーバーのサンプルコードを追加

---
+++ timeserver/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"flag"
+	"io"
+	"log"
+	"net"
+	"os"
+	"time"
+)
+
+func handleConn(c net.Conn) {
+	defer c.Close()
+
+	for {
+		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+		if err != nil {
+			return
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func server() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		handleConn(conn)
+	}
+}
+
+func mustCopy(dst io.Writer, src io.Reader) {
+	if _, err := io.Copy(dst, src); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func client() {
+	log.Print("start client")
+	conn, err := net.Dial("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	mustCopy(os.Stdout, conn)
+}
+
+func main() {
+	s := flag.Bool("s", false, "サーバーとして起動します")
+	flag.Parse()
+	log.Print(*s)
+
+	if *s {
+		server()
+	} else {
+		client()
+	}
+}
